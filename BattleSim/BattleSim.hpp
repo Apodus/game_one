09c37c4c207c33dfd3b1ebbe0d35f5a5867055ef
makush <maakush@gmail.com>
2017-01-17 18:57:19 +0200

    Split BattleSim to async and sync classes

--- BattleSim/BattleSim.hpp
+++ BattleSim/BattleSim.hpp
@@ -13,19 +13,12 @@ namespace bs
 	public:
 
 		// Note: ownership of 'battle' is given to BattleSim until IsComplete() is true
-		BATTLESIM_API BattleSim(Battle& battle, Field::StreamingMode mode = Field::StreamingMode::Enabled);
+		BATTLESIM_API BattleSim(Battle& battle, Field::StreamingMode mode = Field::StreamingMode::Disabled);
 
 		BATTLESIM_API ~BattleSim();
-
-		// Resolves given battle asynchronously. 
-		// Simulation will run max given milliseconds forward until Simulate() needs to be called
-		BATTLESIM_API void ResolveAsync(size_t milliseconds);
-		
+	
 		// Resolves given battle. 
 		BATTLESIM_API void Resolve();
-
-		// Move simulation forward given number of milliseconds, only needed for manual mode
-		BATTLESIM_API void Simulate(size_t milliSeconds);
 
 		static BATTLESIM_API Battle Generate();
 
@@ -37,21 +30,11 @@ namespace bs
 
 		BATTLESIM_API void Cancel(); // Cancels simulation
 
-	private:		
-		void Run();
-
+	protected:		
 		void RunUntilComplete();
 
 		Field myField;
-
-		double myTotalTime = 0; // Total time simulated
-		double myTimeAccu = 0; // Time requested to be simulated, but could not fit in time step
-
 		Battle& myBattle;
-		std::thread myThread;
-		std::mutex myMutex;
-		std::condition_variable myCV;
 		std::atomic<bool> myActiveFlag;
-		size_t myTimeToSimulate;
 	};
 }
